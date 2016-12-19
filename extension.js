e1694407f006eb2fb76ca77314c7449dc64089fe
Jorge Rebocho <jorge.rebocho@gmail.com>
2016-12-19 00:07:02 +0000

    Initial commit of the solution

---
+++ extension.js
@@ -0,0 +1,37 @@
+var vscode = require('vscode')
+// import { vscode } from 'vscode';
+var Chance = require('chance')
+
+const activate = (context) => {
+  console.log('Congratulations, your extension "vscode-random" is now active!')
+  var chance = new Chance()
+
+  var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    const randomNumber = chance.integer()
+    const insertText = `Hello World ${randomNumber}!`
+
+    const editor = vscode.window.activeTextEditor
+
+    if (!editor) {
+      vscode.window.showErrorMessage('No editor found!')
+      return
+    }
+
+    const cursorPosition = editor.selection.active
+
+    var startPosition = cursorPosition
+
+    editor.edit((builder) => {
+      builder.insert(startPosition, insertText)
+    })
+  })
+
+  context.subscriptions.push(disposable)
+}
+
+exports.activate = activate
+
+// function deactivate() {
+// }
+
+// exports.deactivate = deactivate;
