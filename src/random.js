454720bf642976d58d099863bd532d7399a19458
Jorge Rebocho <jorge.rebocho@gmail.com>
2016-12-28 23:51:28 +0000

    Add function to generate random string

--- src/random.js
+++ src/random.js
@@ -1,6 +1,12 @@
 import Chance from 'chance'
 const chance = new Chance()
 
+const DEFAULT_STRING_LENGTH = 10
+
 export const randomByte = () => {
   return chance.integer({min: 0, max: 255}).toString()
+}
+
+export const randomString = (length = DEFAULT_STRING_LENGTH) => {
+  return chance.word({length})
 }
