9399d0742bc14210f120e6594004882a5ee8f28a
Jorge Rebocho <jorge.rebocho@gmail.com>
2016-12-29 00:03:09 +0000

    Add command with input to generate random string with custom length

--- src/commands.js
+++ src/commands.js
@@ -1,5 +1,19 @@
-import { randomByte } from './random'
+import {
+  randomByte,
+  randomString
+} from './random'
+import { isNumber } from './validations'
 
 export const extensionCommands = [
   { key: 'extension.randomByte', callback: randomByte }
+]
+
+export const extensionCommandsWithInput = [
+  {
+    key: 'extension.randomStringCustomLength',
+    callback: randomString,
+    prompt: 'Enter the length of the string',
+    validation: isNumber,
+    errorMsg: 'The input value must be a number'
+  }
 ]
