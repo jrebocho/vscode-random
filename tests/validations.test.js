24317f1e670629a17728bd23d2fd1ead2536aca0
Jorge Rebocho <jorge.rebocho@gmail.com>
2017-01-07 19:48:07 +0000

    Add function to validate if a string is not empty

--- tests/validations.test.js
+++ tests/validations.test.js
@@ -1,4 +1,4 @@
-import { isNumber } from '../src/validations'
+import { isNumber, isValid } from '../src/validations'
 
 describe('>>>>> Validator Functions Tests', () => {
   describe('.isNumber', () => {
@@ -24,6 +24,52 @@ describe('>>>>> Validator Functions Tests', () => {
       const valueToTest = 'false'
 
       expect(isNumber(valueToTest)).toBeFalsy()
+    })
+  })
+
+  describe('.isValid', () => {
+    it('returns false if input value is null', () => {
+      const valueToTest = null
+
+      expect(isValid(valueToTest)).toBeFalsy()
+    })
+
+    it('returns false if input value is undefined', () => {
+      const valueToTest = undefined
+
+      expect(isValid(valueToTest)).toBeFalsy()
+    })
+
+    it('returns false if input value is an empty string', () => {
+      const valueToTest = ''
+
+      expect(isValid(valueToTest)).toBeFalsy()
+    })
+
+    it('validates a non empty string', () => {
+      const valueToTest = 'true value'
+
+      expect(isValid(valueToTest)).toBeTruthy()
+    })
+
+    describe('when maxLength is specified', () => {
+      it('return false if string has length greater than maxLength', () => {
+        const valueToTest = 'true value greater'
+
+        expect(isValid(valueToTest, 10)).toBeFalsy()
+      })
+
+      it('validates a non empty string with length equal to maxLength', () => {
+        const valueToTest = 'true value'
+
+        expect(isValid(valueToTest, 10)).toBeTruthy()
+      })
+
+      it('validates a non empty string with length less than maxLength', () => {
+        const valueToTest = 'true val'
+
+        expect(isValid(valueToTest, 10)).toBeTruthy()
+      })
     })
   })
 })
